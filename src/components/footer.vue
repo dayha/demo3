99974a3b6a605b2bc221bc9db10add6074014460
new Boy <361146992@qq.com>
2018-09-19 23:39:07 +0800

    diyici
    diyic

---
+++ src/components/footer.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="footer fiexd">
+		<ul>
+			<li><router-link to='/'>首页</router-link></li>
+			<li><router-link to='/manage'>我的</router-link></li>
+		</ul>
+	</div>
+</template>
+<style scoped>
+	li a{
+		display:inline-block;width: 100%;height: 100%;
+	}
+	.footer{
+		width: 100%;height:50px;bottom: 0;
+	}
+	ul{
+		display: flex;height: 100%;line-height: 50px;
+	}
+	ul li{
+		flex: 1;background: #f1f1f1;
+	}
+	.isIndex li:first-child{
+		background: #d3d3d3;
+	}
+	.isIndex li a:first-child{
+		color: #41b833;
+	}
+	.isManage li:first-child{
+		background: #d3d3d3;
+	}
+	.isManage li a:first-child{
+		color: #41b833;
+	}
+
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		第一次使用
+		<footer-nav></footer-nav>
+	</div>
+</template>
+<script>
+	import FooterNav from './footer.vue'
+	export default{
+		components:{
+			FooterNav
+		}
+	}
+</script>
